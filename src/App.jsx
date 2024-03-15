07df9129638061a307d019f34e0d08ddcd3ccaa0
Lahoucine Ijioui <ije1work@gmail.com>
2024-03-15 11:22:13 +0000

    add tailwindcss colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import './index.css';
-import { Button } from './components/ui/button';
-import { CarouselDemo } from './components/demo/CarousselDemo';
-
 
 function App() {
   return (
-    <div className='px-36'>
+    <div className=''>
       <h1>Printnoon</h1>
-      <Button>Schadcn</Button>
-      <CarouselDemo></CarouselDemo>
     </div>
   )
 }
