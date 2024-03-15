a39d9ffc3b84f3a72ae0d44af688426c70b380cd
Lahoucine Ijioui <ije1work@gmail.com>
2024-03-15 14:33:20 +0000

    add logo to auth navigation bar

--- src/components/AuthNavbar.jsx
+++ src/components/AuthNavbar.jsx
@@ -3,8 +3,10 @@ import Logo from "./nav-parts/Logo";
 
 function AuthNavbar() {
   return (
-    <nav>
-      <Logo />
+    <nav className="py-2 px-4 border-b-4">
+      <div className="w-28">
+        <Logo />
+      </div>
     </nav>
   )
 }
--- src/components/nav-parts/Logo.jsx
+++ src/components/nav-parts/Logo.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Link } from "react-router-dom";
 import logo from "../../assets/main-logo-printnoon.png";
 
@@ -7,7 +7,7 @@ import logo from "../../assets/main-logo-printnoon.png";
 const Logo = () => {
   return (
     <Link to="/">
-        <img className="h-8" src={logo}></img>
+        <img className="" src={logo}></img>
     </Link>
   )
 }
