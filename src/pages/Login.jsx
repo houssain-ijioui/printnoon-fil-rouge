6697336b0033db5a57f535a6fbc982108ad244cb
Lahoucine Ijioui <ije1work@gmail.com>
2024-03-20 10:17:29 +0000

    login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import AuthNavbar from '@/components/AuthNavbar';
+import sideImage from "@/assets/auth/auth 1.png"
+import { Link } from 'react-router-dom';
+import InputField from '@/components/ui/InputField';
+import AuthButton from '@/components/ui/AuthButton';
 
 
-const Login = () => {
+const Signup = () => {
   return (
-    <h1>Login</h1>
+    <div className='h-full flex flex-col'>
+      <AuthNavbar />
+      <div className='flex flex-row justify-between grow'>
+        <div className='py-16 px-20'>
+          <h1 className='text-3xl font-semibold tracking-wide mb-3 w-5/5'>Emballage de fole digne de ce nom, trie !</h1>
+          <h4 className='text-secondBlue font-semibold text-base mb-3'>Connectez-vous pour suiver vos conceptions et vos commandes, le tout en un seul endroit.</h4>
+          <div className='flex flex-row mb-3'>
+            <h4 className='font-semibold'>Déja inscris ?</h4>
+            <Link to="/signup"><h4 className='text-thirdBlue font-semibold ml-4 hover:underline'>Se connecter</h4></Link>
+          </div>
+          <form className=''>
+            <InputField placeholderText={"Email"} type={"email"} />
+            <InputField placeholderText={"Password"} type={"password"} />
+            <AuthButton text={"Login"} />
+          </form>
+        </div>
+        <div className='bg-firstBlue py-4 pl-8'>
+          <img src={sideImage} />
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default Login;
+export default Signup
