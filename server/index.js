b791fb3b38c42406ad63998eb5a03e08f42755c1
Lahoucine Ijioui <ije1work@gmail.com>
2024-03-20 12:40:19 +0000

    comment operations

--- server/index.js
+++ server/index.js
@@ -9,11 +9,16 @@ const app = express();
 
 app.use(express.json());
 
+// connect db
 const db = new DB(process.env.MONGO_URI)
 db.connect()
 
+// authetification routes
 app.use('/api/auth', authRoutes);
 
+
+// non existing routes
 app.use(routeNotFound);
+
 
 app.listen(8000, console.log("Server Running in 8000"));
