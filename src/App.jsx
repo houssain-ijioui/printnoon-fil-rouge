173af2fc77693d92abf77a2e4b9234baf7ea0b5f
Lahoucine Ijioui <ije1work@gmail.com>
2024-03-22 12:28:44 +0000

    configure routes and redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import './index.css';
-import { Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/login' element={<Login />} />
-      <Route path='/signup' element={<Signup />} />
-    </Routes>
+    <Outlet />
   )
 }
 
