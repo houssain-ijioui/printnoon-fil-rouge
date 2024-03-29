8a3271d1134baa4668da7d994e079ba9b1f4dab9
Lahoucine Ijioui <ije1work@gmail.com>
2024-03-29 10:59:10 +0000

    final check for conceptions page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import picture from "../assets/image2.png"
 const Card = () => {
   return (
     <div className='flex flex-col border border-thirdBlue bg-slate-200 rounded-xl'>
-        <img className='h-9/12 self-center border border-b-thirdBlue rounded-xl w-full p-3 rounded-lg' src={picture}/>
+        <img className='h-9/12 self-center border border-b-thirdBlue rounded-xl w-full p-3' src={picture}/>
         <div className='p-3'>
           <h2 className='font-semibold text-firstBlue'>Societe PlusDig</h2>
           <h4 className='font-light'>12-07-2012</h4>
