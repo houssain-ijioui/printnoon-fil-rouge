5152f0fd98ea17585e34596684520beb31308ab8
Lahoucine Ijioui <ije1work@gmail.com>
2024-04-01 10:46:01 +0000

    add order schema

---
+++ server/models/order.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+
+const orderSchema = new mongoose.Schema({
+    nom: {
+        type: String,
+        required: true
+    },
+    dimensions: {
+        type: String,
+        required: true
+    },
+    papier: {
+        type: String,
+        required: true
+    },
+    grammage: {
+        type: String,
+        required: true
+    },
+    coins: {
+        type: String,
+        required: true
+    },
+    orientation: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+
+
+const Order = mongoose.model('Order', orderSchema);
+
+export default Order;
